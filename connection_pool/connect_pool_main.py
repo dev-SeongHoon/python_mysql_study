3093c120dd5701255fdca4f57e7f119bf99d7acf
dev-SeongHoon <rpgmaker1004@gmail.com>
2019-12-12 19:55:04 +0900

    Python Mysql connection pool 연결작업 완료

--- connection_pool/connect_pool_main.py
+++ connection_pool/connect_pool_main.py
@@ -3,6 +3,7 @@ import inspect
 from connection.connection_study01 import connect, connect_use_config
 from connection.python_mysql_dbconfig import read_db_config
 from connection_pool.connection_pool_study import DatabaseConnectionPool
+from connection_pool.connection_pool_study02 import ExplicitlyConnectionPool, get_implicitly_connection
 
 
 def connect_pool01():
@@ -12,9 +13,27 @@ def connect_pool01():
     connection.close()
 
 
-if __name__ == "__main__":
-     for i in range(20):
-       connect_pool01()
+def explicitly_connection_pool():
+    print("\n== {}() ==".format(inspect.stack()[0][3]))
+    connectioPool = ExplicitlyConnectionPool.get_instance()
+    print("ConnectionPool {}".format(connectioPool))
+    connection = connectioPool.get_connection()
+    print("Connection {}".format(connection))
+    connection.close()
 
-    #explicitly_connection_pool()
-    #implicitly_connection_pool()
+
+def implicitly_connection_pool():
+    print("\n== {}() ==".format(inspect.stack()[0][3]))
+    connectioPool = get_implicitly_connection()
+    print("ConnectionPool {}".format(connectioPool))
+    connection = connectioPool.get_connection()
+    print("Connection {}".format(connection))
+    connection.close()
+
+
+if __name__ == "__main__":
+     #for i in range(20):
+     #  connect_pool01()
+     #
+    explicitly_connection_pool()
+    implicitly_connection_pool()
